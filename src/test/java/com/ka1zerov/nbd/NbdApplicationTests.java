77d542375a7e8b6024045df5afa2e860095b69e7
Tymofii Skrypko <skrypko270901@gmail.com>
2025-01-18 17:02:22 +0100

    Initial Commit

---
+++ src/test/java/com/ka1zerov/nbd/NbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ka1zerov.nbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
